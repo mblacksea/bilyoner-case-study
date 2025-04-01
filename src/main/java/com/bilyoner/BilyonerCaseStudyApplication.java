746effe9c9a554e2451460c15ec85e589abf969f
Mustafa Karadeniz <mustafa.karadeniz@all-for-one.com>
2025-04-01 14:25:20 +0300

    Enc database password with Jasypt library

--- src/main/java/com/bilyoner/BilyonerCaseStudyApplication.java
+++ src/main/java/com/bilyoner/BilyonerCaseStudyApplication.java
@@ -1,9 +1,11 @@
 package com.bilyoner;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class BilyonerCaseStudyApplication {
 
     public static void main(String[] args) {
